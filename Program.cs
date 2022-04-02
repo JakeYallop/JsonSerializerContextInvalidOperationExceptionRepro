5abf45f81a062f724a41976d0d82c972e7c81018
JakeYallop <30874283+JakeYallop@users.noreply.github.com>
2022-04-02 15:07:38 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
+
+public class MyClass
+{
+    //[Compare("Password", ErrorMessageResourceType = "")]
+    //[Required(ErrorMessage = "")]
+    [Derived(ErrorMessage = "")]
+    public string Property { get; set; } = null!;
+}
+
+public abstract class BaseAttribute : Attribute
+{
+    public string ErrorMessage { get; set; } = null!;
+}
+
+[AttributeUsage(AttributeTargets.All)]
+public class DerivedAttribute : BaseAttribute
+{
+}
+
+
+[JsonSourceGenerationOptions]
+[JsonSerializable(typeof(MyClass))]
+public partial class JsonContext : JsonSerializerContext
+{
+
+}
