d1a62ddb0f0816595498c161092397c560cec2d5
JakeYallop <30874283+JakeYallop@users.noreply.github.com>
2022-04-02 15:13:35 +0100

    updates

--- Program.cs
+++ Program.cs
@@ -2,24 +2,27 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Text.Json.Serialization;
 
+Console.WriteLine("Hello world!");
+
 public class MyClass
 {
-    //[Compare("Password", ErrorMessageResourceType = "")]
-    //[Required(ErrorMessage = "")]
-    [Derived(ErrorMessage = "")]
+    [Derived(BaseProperty = "")] //source generator does not compile
+    //[Derived(DerivedProperty = "", BaseProperty = "")] //does not compile
+    //[Derived] //compiles fine
+    //[Derived(DerivedProperty = "")] //compiles fine
     public string Property { get; set; } = null!;
 }
 
 public abstract class BaseAttribute : Attribute
 {
-    public string ErrorMessage { get; set; } = null!;
+    public string BaseProperty { get; set; } = null!;
 }
 
 [AttributeUsage(AttributeTargets.All)]
 public class DerivedAttribute : BaseAttribute
 {
+    public string DerivedProperty { get; set; } = null!;
 }
-
 
 [JsonSourceGenerationOptions]
 [JsonSerializable(typeof(MyClass))]
